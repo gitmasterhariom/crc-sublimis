76d6d2c9dd8e19b6a076d74c561589a51ffbe16c
gitmasterhariom <43899492+gitmasterhariom@users.noreply.github.com>
2018-10-11 21:24:46 +0530

    Create mail.php

---
+++ mail.php
@@ -0,0 +1,25 @@
+<?if($_POST[submit]=='submit'){
+$name=$_POST[name];
+$email=$_POST[email];
+$phon=$_POST[phon];
+$message=$_POST[msg];
+$emailSubject = 'Enquiry Now';
+
+$mailto ='ravi01989@yahoo.com';
+$ip = $_SERVER['REMOTE_ADDR']; 
+$body = <<<EOD
+<br><hr><br>
+ <br>
+Name* 		 :     $name     <br>
+Mobile No*       :  $phon  <br>      
+Email Id         :   $email   <br>         
+Message          :   $message   <br>      
+ <br>
+EOD;
+
+$headers = "From: realbrick04@gmail.com\r\n"; // This takes the email and displays it as who this email is from.
+$headers .= "Content-type: text/html\r\n"; // This tells the server to turn the coding into the text.
+$success = mail($mailto, $emailSubject, $body, $headers); // This tells the server what to send.
+$success ? "" : "Mail failed";
+
+}?>
